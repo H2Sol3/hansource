795a0aa17e23b8926af4c0ed08663c597a5997c2
thfas <thfas@DESKTOP-BQM1K2Q>
2020-05-21 00:02:06 +0900

    전에 했던거 합쳐서 한꺼번에 올리기.

--- src/test/Lotto.java
+++ src/test/Lotto.java
@@ -2,9 +2,60 @@ package test;
 
 //로또번호 생성
 public class Lotto {
-
 	public static void main(String[] args) {
-		System.out.println("로또번호 생성하기!");
+		System.out.println("로또번호 생성");
+		int[] num = new int[45];
+		int[] lotto = new int[6];
+		
+		for(int i=0;i<num.length;i++) {
+			num[i]=(int)(Math.random()*45)+1;
+			//중복제거
+			for(int j=0;j<i;j++) {
+				if(num[i]==num[j]) {
+					i--;					
+				}
+			}
+		}
+		
+		for(int i=0;i<lotto.length;i++) {
+			lotto[i]=num[i];
+		}
+		//정렬
+		for(int i=0;i<lotto.length;i++) {
+			for(int j=0;j<i;j++) {
+				if(lotto[i]<lotto[j]) {
+					int tmp=lotto[i];
+					lotto[i]=lotto[j];
+					lotto[j]=tmp;
+				}
+			}
+		}
+		
+		for(int l:lotto) {
+			System.out.print(l+" ");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		/*System.out.println("로또번호 생성하기!");
 		int[] lotto = new int[6]; // 당첨번호 6개 넣을 공간.
 		int[] num = new int[6]; // 번호뽑아서 정렬하기.
 		int tmp; // 정렬하기위해서 하나 선언
@@ -31,7 +82,46 @@ public class Lotto {
 		
 		for (int i = 0; i < num.length; i++) {
 			System.out.print(num[i] + " ");
+		}*/
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	/*	
+		
+		
+	
+		System.out.println("로또번호 출력");
+		int [] lotto = new int[6];
+		for(int i=0;i<lotto.length;i++) {
+			lotto[i]=(int)(Math.random()*45+1);
+			for(int j=0;j<i;j++) {
+				if(lotto[i]==lotto[j]) {
+					i--;
+				}
+			}
 		}
+		
+		//정렬
+		for(int i=0;i<lotto.length;i++) {
+			for(int j=0;j<i;j++) {
+				if(lotto[i]<lotto[j]) {
+					int tmp=lotto[i];
+					lotto[i]=lotto[j];
+					lotto[j]=tmp;
+				}
+			}
+		}
+		
+		for(int i=0;i<lotto.length;i++) {
+			System.out.print(lotto[i]+" ");
+		}*/
 	}
 
 }
--- src/test/String_length.java
+++ src/test/String_length.java
@@ -2,17 +2,6 @@ package test;
 
 import java.util.Scanner;
 
-/*
-단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
-
-제한사항
-s는 길이가 1 이상, 100이하인 스트링입니다.
-
-입출력 예
-s	return
-abcde	c
-qwer	we
- */
 public class String_length {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
@@ -36,6 +25,17 @@ public class String_length {
 
 
 
+/*
+단어 s의 가운데 글자를 반환하는 함수, solution을 만들어 보세요. 단어의 길이가 짝수라면 가운데 두글자를 반환하면 됩니다.
+
+제한사항
+s는 길이가 1 이상, 100이하인 스트링입니다.
+
+입출력 예
+s	return
+abcde	c
+qwer	we
+ */
 
 
 
